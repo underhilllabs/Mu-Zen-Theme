974c73fac973545b01236aa33acc587e4528a2b8
Gray Jay <grayjay@denvertech.org>
2011-12-05 02:29:35 -0500

    changed theme-settings to use mu instead of STARTERKIT in fnc names

--- theme-settings.php
+++ theme-settings.php
@@ -7,15 +7,15 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function STARTERKIT_form_system_theme_settings_alter(&$form, &$form_state)  {
+function mu_form_system_theme_settings_alter(&$form, &$form_state)  {
 
   // Create the form using Forms API: http://api.drupal.org/api/7
 
   /* -- Delete this line if you want to use this setting
-  $form['STARTERKIT_example'] = array(
+  $form['mu_example'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('STARTERKIT sample setting'),
-    '#default_value' => theme_get_setting('STARTERKIT_example'),
+    '#title'         => t('mu sample setting'),
+    '#default_value' => theme_get_setting('mu_example'),
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
   // */
